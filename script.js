09ec5dba83a6efe5917f616c484f39e3ac87d761
Isidora Stanisavljevska <137770804+moonrose93@users.noreply.github.com>
2023-08-11 21:11:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,17 @@ const numberRegex = /^\d*$/;/*In this code, I've attached an input event listene
 \d+: Match one or more digits (0-9)
 $: End of the string*/
 
+// Card number should be 16 digits
+const cardNumberRegex = /^\d{16}$/;
+// Month should be 2 digits
+const monthRegex = /^\d{2}$/;
+// Year should be 2 digits
+const yearRegex = /^\d{2}$/;
+// CVC should be 3 digits
+const cvcRegex = /^\d{3}$/;
+// Name should contain only letters
+const nameRegex = /^[A-Za-z\s]+$/;
+
 
 // Function to set outline color-we use it for every input = by calling this function inside the eventlistener and the input.
 function setOutlineColor(inputElement, color) {
@@ -52,24 +63,26 @@ function setOutlineColor(inputElement, color) {
 }
 
 
-// Event listener to validate and change outline color
+// Event listener to validate and change outline color-if card input vale is not equal to the regex patter then set outline color to the cardinput red elese set it blue
 cardInput.addEventListener("input", () => {
-  if (!numberRegex.test(cardInput.value)) {
+  if (!cardNumberRegex.test(cardInput.value)) {
     setOutlineColor(cardInput, "red");   
   } else {
     setOutlineColor(cardInput, "blue"); // Reset to default or remove outline
   }
 });
 
-
+//if name input value is not empty then the name input to turn blue else to be normal
 nameInput.addEventListener("keyup",()=>{
-  if(nameInput.value !== ""){
-    setOutlineColor(nameInput,"blue");
+  if(!nameRegex.test(nameInput.value)){
+    setOutlineColor(nameInput,"red");
   }else{
-    setOutlineColor(nameInput, "");
+    setOutlineColor(nameInput, "blue");
   }
 })
 
+
+//if nameinput value i quual to an empty string then we set the defualt text for the card else we set the the name input value
 nameInput.addEventListener("keyup",()=>{
   if(nameInput.value ===""){
     cardNameDesktop.innerText = "Jane Appleseed";
@@ -80,7 +93,7 @@ nameInput.addEventListener("keyup",()=>{
   }
 })
 
-// Event listener to update card numbers
+// Event listener to update card numbers - if card input is empty then cardnum to be set as its set  else the cardnum to be equal to the input value.
 cardInput.addEventListener("keyup", () => {
   if (cardInput.value === "") {
     cardNumDesktop.innerText = "0000 0000 0000 0000";
@@ -107,7 +120,7 @@ monthInput.addEventListener("keyup", () => {
 const inputBlankMessage1 = document.getElementById("blank1");
 
 monthInput.addEventListener("keyup",()=>{
-  if(monthInput.value === ""){
+  if(!monthRegex.test(monthInput.value)){
     setOutlineColor(monthInput, "red");
     inputBlankMessage.style.display = "block";
 
@@ -122,7 +135,7 @@ monthInput.addEventListener("keyup",()=>{
 const inputBlankMessage2 = document.getElementById("blank2");
 
 yearInput.addEventListener("keyup", ()=>{
-  if(yearInput.value === ""){
+  if(!yearRegex.test(yearInput.value)){
     setOutlineColor(yearInput, "red");
     inputBlankMessage2.style.display = "block";
   }else{
@@ -155,21 +168,22 @@ cvcInput.addEventListener("keyup", () => {
     const container = document.querySelector(".right");
     
   
-        confirmBtn.addEventListener("click", () => {
-          // Check if all input fields are filled
-          const allInputsFilled =
-            nameInput.value.trim() !== "" &&
-            cardInput.value.trim() !== "" &&
-            monthInput.value.trim() !== "" &&
-            yearInput.value.trim() !== "" &&
-            cvcInput.value.trim() !== "";
-      
-          if (allInputsFilled) {
-            // Display the popup and hide the container's right side
-            popup.style.display = "block";
-            container.style.display = "none";
-          }
-        });
+    confirmBtn.addEventListener("click", () => {
+      // Check if all input fields are filled and meet the regex requirements
+      const allInputsFilledAndValid =
+        nameRegex.test(nameInput.value) &&
+        cardNumberRegex.test(cardInput.value) &&
+        monthRegex.test(monthInput.value) &&
+        yearRegex.test(yearInput.value) &&
+        cvcRegex.test(cvcInput.value);
+    
+      if (allInputsFilledAndValid) {
+        // Display the popup and hide the container's right side
+        popup.style.display = "block";
+        container.style.display = "none";
+      }
+    });
+    
       
         continueBtn.addEventListener("click", () => {
           // Hide the popup and show the container's right side
